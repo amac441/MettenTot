b86d1cf3aaad083dac08c2a60f34661c131a323b
amac441 <ajkrell@yahoo.com>
2014-02-03 20:58:38 -0600

    Updated CSS

--- MettenTot/MettenTot/settings.py
+++ MettenTot/MettenTot/settings.py
@@ -67,6 +67,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    "C:/Users/Andrew/Documents/Visual Studio 2013/Projects/MettenTot/MettenTot/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- MettenTot/blog/views.py
+++ MettenTot/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from blog.models import Post #need to import the model for the blog
 
 def index(request):
     return render_to_response('index.html')
         #takes a parameter, request, which is an object that has information about the 
         #user requesting the page from the browser. 
         #The function's response is to simply render the index.html template
+
+def tagpage(request, tag):
+    posts = Post.objects.filter(tags__name=tag)
+    return render_to_response("tagpage.html", {"posts":posts, "tag": tag})
+
